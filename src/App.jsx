f316de44b81b81c8811957efbeadda36b1329b24
erikaahlstrom <info@erikasoldh.se>
2022-03-13 15:09:17 +0100

    fix: print a MovieCard in grid system for each movie .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect} from 'react';
 import './App.css';
+import ActionAreaMovieCard from './components/MovieCard';
 import { getPopularMoviesFetch } from './fetches/fetches'
+import { experimentalStyled as styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
 
 function App() {
@@ -19,12 +24,15 @@ function App() {
 
   return (
     <div className="App"> 
-       {movies?.map(movie => (
-         <>
-        <p>{movie?.original_title}</p>
-        <img src={`https://image.tmdb.org/t/p/w500${movie?.poster_path}`} alt={movie?.original_title} />
-        </>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+       {movies?.map((movie, index) => (
+         <Grid item xs={2} sm={4} md={4} key={index}>
+        <ActionAreaMovieCard movieProps={movie}/>
+        </Grid>
       ))} 
+      </Grid>
+    </Box>
     </div>
   );
 }
