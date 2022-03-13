02be56c9955397119f912430f691ae796344c6f4
erikaahlstrom <info@erikasoldh.se>
2022-03-13 14:13:18 +0100

    fix: set up getPopularMovies fetch to App.js.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, { useState, useEffect} from 'react';
 import './App.css';
+import { getPopularMoviesFetch } from '../src/fetches/fetches'
+
 
 function App() {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getPopularMovies()
+  }, [])
+  
+  async function getPopularMovies() {
+    const getTop100MoviesResponse = await getPopularMoviesFetch()
+    setMovies(getTop100MoviesResponse);
+    console.log(getTop100MoviesResponse.data.results)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
