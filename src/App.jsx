840c89c275d2ef4ecd8f4d165660173205afe0a1
erikaahlstrom <info@erikasoldh.se>
2022-03-13 22:22:32 +0100

    fix: add todo list in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,14 @@ import MoviesPage from './pages/MoviesPage';
 import ResponsiveAppBar from './components/Header';
 import MovieDetailsPage from './pages/MovieDetailsPage';
 
-
+// TODO
+/**********************************
+* Fix so user don't have to click the button again when going back from detail page
+* Refactor to get the the code more DRY in MoviesPage
+* fix so CardActionArea acts lik a react Link, remove useHistory, not accessible
+**********************************/
 
 function App() {
-
 
   return (
     <>
