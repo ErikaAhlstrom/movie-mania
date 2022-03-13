9d6317cf77cf4e5ee6f8d8d5db88cd014dff7132
erikaahlstrom <info@erikasoldh.se>
2022-03-13 22:23:49 +0100

    refactor: change fetch name, more informative.

--- src/fetches/fetches.js
+++ src/fetches/fetches.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 const apiKey = process.env.REACT_APP_API_KEY
 
-const url = axios.create({ baseURL: 'https://api.themoviedb.org/3'});
+const url = axios.create({ baseURL: 'https://api.themoviedb.org/3/movie'});
 
 /**********************************
  * MOVIE FETCHES GOES HERE
 **********************************/
 
-export const getPopularMoviesFetch = () => url.get(`/movie/popular?api_key=${apiKey}&language=en-US&page=1`);
-export const getMovieByIdFetch = (movieId) => url.get(`/movie/${movieId}?api_key=${apiKey}&language=en-US`);
+export const getPopularMoviesByPageIdFetch = (pageId) => url.get(`/popular?api_key=${apiKey}&language=en-US&page=${pageId}`);
+export const getMovieByIdFetch = (movieId) => url.get(`/${movieId}?api_key=${apiKey}&language=en-US`);
 
 /**********************************
  * USER FETCHES GOES HERE
