773ff0055015f779dad0045067da4feb203c7a89
erikaahlstrom <info@erikasoldh.se>
2022-03-13 17:31:48 +0100

    fix: create MovieDetailsPage.

---
+++ src/pages/MovieDetailsPage.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import Box from '@mui/material/Box';
+
+
+const MovieDetailsPage = ({moviesProps}) => {
+  return (
+    
+    <Box >
+        Hello Details
+    </Box>
+  )
+}
+
+export default MovieDetailsPage
+
+
