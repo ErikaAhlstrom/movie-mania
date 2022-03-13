3c23565595bf8b6fce40dce6468d8b820ad35fa9
erikaahlstrom <info@erikasoldh.se>
2022-03-13 18:58:37 +0100

    refactor: break out MoviesPage from App.js.

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,22 @@
-import React, { useState, useEffect} from 'react';
-import {Route, Switch} from 'react-router-dom';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import { getPopularMoviesFetch } from './fetches/fetches'
-import Box from '@mui/material/Box';
-import ResponsiveAppBar from './components/Header';
-import CircularProgress from '@mui/material/CircularProgress';
 import MoviesPage from './pages/MoviesPage';
+import ResponsiveAppBar from './components/Header';
 import MovieDetailsPage from './pages/MovieDetailsPage';
-import Button from '@mui/material/Button';
 
 
 
 function App() {
 
-  const [movies, setMovies] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-
-/*   useEffect(() => {
-    getPopularMovies()
-  }, []) */
-  
-  async function getPopularMovies() {
-    const getPopularoviesResponse = await getPopularMoviesFetch()
-    setMovies(getPopularoviesResponse.data.results);
-  }
-
-  function handleClick() {
-    setIsLoading(true)
-    setTimeout(() => {getPopularMovies()}, 2000);
-  }
 
   return (
     <>
     <ResponsiveAppBar />
     <Switch>
       <Route path="/movies/:id" component={MovieDetailsPage}>
-
       </Route>
-      <Route path="/">
-        <Box sx={{m:4, display: "flex", justifyContent: "center", flexDirection: 'column'}} className="App"> 
-          { !movies && !isLoading &&
-          <Button 
-            onClick={() => {
-              handleClick()
-            }}
-            variant="contained">
-            Get Movies
-          </Button>
-          }
-
-          {isLoading && !movies &&
-          <Box sx={{display: "flex", justifyContent: "center", p:5}}>
-            <CircularProgress/>
-          </Box>}
-          {movies &&
-            <MoviesPage moviesProps={movies}/>
-          }
-        </Box>
+      <Route exact path="/" component={MoviesPage}>
       </Route>
     </Switch>
     </>
