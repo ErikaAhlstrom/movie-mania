e8a1a85ee060104f223ba5e8c173e936e6137c16
erikaahlstrom <info@erikasoldh.se>
2022-03-14 09:12:49 +0100

    fix: add todos in app.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import MoviesPage from './pages/MoviesPage';
 import ResponsiveAppBar from './components/Header';
 import MovieDetailsPage from './pages/MovieDetailsPage';
 
-// TODO
+//TODO
 /**********************************
-* Fix so user don't have to click the button again when going back from detail page
-* Refactor to get the the code more DRY in MoviesPage
-* fix so CardActionArea acts lik a react Link, remove useHistory, not accessible
+* Fix so user don't have to click the button again when going back from detail page.
+* Refactor to get the the code more DRY in MoviesPage.
+* fix so CardActionArea acts like a react Link, remove useHistory, not accessible now.
+* Add theme and theme provider for more long term way of styling.
+* Set up linting
 **********************************/
 
 function App() {
